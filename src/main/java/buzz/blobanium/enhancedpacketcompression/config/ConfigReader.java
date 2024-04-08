ebc6e79d739a1be02246ca617462ff4d0364fbea
Blobanium <45613796+Blobanium@users.noreply.github.com>
2024-04-07 22:55:42 -0700

    Initial commit

---
+++ src/main/java/buzz/blobanium/enhancedpacketcompression/config/ConfigReader.java
@@ -0,0 +1,55 @@
+package buzz.blobanium.enhancedpacketcompression.config;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+public class ConfigReader {
+    public static final Logger LOGGER = LogManager.getLogger("EnhancedPacketCompression - Config");
+    //variables
+    public static boolean needsConfigRefresh = false;
+    public static boolean refreshingConfig = false;
+
+    //configs
+    public static int packetCompressionLevel = 2;
+
+    public static void configRegister(boolean initialize){
+        LOGGER.debug("Registering config..");
+        SimpleConfig CONFIG = SimpleConfig.of("enhancedpacketcompression").provider(ConfigReader::ltProvider).request();
+
+        if(initialize) {
+            packetCompressionLevel = CONFIG.getOrDefault("packet_compression_level", packetCompressionLevel);
+        }
+
+        LOGGER.debug("Regestering done!");
+    }
+
+    private static String ltProvider(String filename) {
+        return "#Bumble Kingdom Core Config File."
+                + "\npacket_compression_level=" + packetCompressionLevel;
+
+    }
+
+    public static void refreshConfig(){
+        if(needsConfigRefresh) {
+            refreshingConfig = true;
+            try {
+                if (!Files.deleteIfExists(FabricLoader.getInstance().getConfigDir().resolve("MineclubExpanded.properties"))) {
+                    LOGGER.error("Config file not found. Please ensure the path to the config is correct.\n" + FabricLoader.getInstance().getConfigDir().resolve("LoadingTimer.properties"));
+                }
+            } catch (IOException e) {
+                LOGGER.fatal("Config Refresh Failed due to a IOException, please report this on our issues thread.", e);
+            }
+            configRegister(false);
+            refreshingConfig = false;
+        }
+    }
+
+    public static void onConfigSave(){
+        needsConfigRefresh = true;
+    }
+}
---
+++ src/main/java/buzz/blobanium/enhancedpacketcompression/mixin/PacketDeflaterMixin.java
@@ -0,0 +1,29 @@
+package buzz.blobanium.enhancedpacketcompression.mixin;
+
+import buzz.blobanium.enhancedpacketcompression.config.ConfigReader;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import net.minecraft.network.PacketDeflater;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.zip.Deflater;
+
+@Mixin(PacketDeflater.class)
+public class PacketDeflaterMixin {
+    @Shadow @Final private Deflater deflater;
+
+    @Inject(at = @At(value = "INVOKE", target = "Ljava/util/zip/Deflater;setInput([BII)V"), method = "encode(Lio/netty/channel/ChannelHandlerContext;Lio/netty/buffer/ByteBuf;Lio/netty/buffer/ByteBuf;)V")
+    private void beforeCompress(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, ByteBuf byteBuf2, CallbackInfo ci){
+        this.deflater.setLevel(ConfigReader.packetCompressionLevel);
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Ljava/util/zip/Deflater;reset()V"), method = "encode(Lio/netty/channel/ChannelHandlerContext;Lio/netty/buffer/ByteBuf;Lio/netty/buffer/ByteBuf;)V")
+    private void afterCompress(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, ByteBuf byteBuf2, CallbackInfo ci){
+        //This mixin is reserved for future usage.
+    }
+}

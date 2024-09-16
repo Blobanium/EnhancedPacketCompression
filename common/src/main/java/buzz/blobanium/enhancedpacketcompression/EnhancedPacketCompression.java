4fbf5db99f359d38a2b83ac956687148741a5b75
Blobanium <45613796+Blobanium@users.noreply.github.com>
2024-09-15 19:52:15 -0700

    Remove system.out.println code

--- common/src/main/java/buzz/blobanium/enhancedpacketcompression/EnhancedPacketCompression.java
+++ common/src/main/java/buzz/blobanium/enhancedpacketcompression/EnhancedPacketCompression.java
@@ -12,7 +12,6 @@ public class EnhancedPacketCompression{
     public static Path configDir;
 
     public static void init() {
-        System.out.println(configDir);
         ConfigReader.configRegister(true);
         LOGGER.info("EnhancedPacketCompression initialized");
     }
